fed78f65139d6601c41e0feeaed5f7fee368e2ae
Cameron Abel <cameronabel@gmail.com>
2022-11-03 00:23:47 -0700

    handle triangle calc and displays

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 // business logic
 
 function isEquilateral(sideA, sideB, sideC) {
-  return sideA === sideB === sideC;
+  console.log("func eq")
+  return (sideA === sideB && sideA === sideC);
 }
 
 function isIsosceles(sideA, sideB, sideC) {
@@ -16,18 +17,45 @@ function isIsosceles(sideA, sideB, sideC) {
   }
 }
 
+function hideResultsAndError () {
+  nullP = document.getElementById("null-error");
+  typeField = document.getElementById("triangle-type");
+  if (!(nullP.classList.contains("hidden"))) {
+    nullP.classList.add("hidden");
+  }
+  if (!(typeField.classList.contains("hidden"))) {
+    typeField.classList.add("hidden");
+  }
+}
+
 function triangulate(event) {
   event.preventDefault();
-  nullP = document.getElementById("null-error")
-  if (!(nullP.classList.contains("hidden"))) {
-    nullP.classList.add("hidden")
-  }
+  hideResultsAndError ();
+  nullP = document.getElementById("null-error");
+  typeField = document.getElementById("triangle-type");
   let sideA = document.getElementById("side-a").value;
   let sideB = document.getElementById("side-b").value;
   let sideC = document.getElementById("side-c").value;
+
   if (sideA === "" || sideB === "" || sideC === "") {
-    nullP.classList.remove("hidden")
+    nullP.classList.remove("hidden");
+    return
   }
+
+  sideA = parseFloat(sideA);
+  sideB = parseFloat(sideB);
+  sideC = parseFloat(sideC);
+
+  let nameField = document.getElementById("triangle-name");
+  if (isEquilateral(sideA, sideB, sideC)) {
+    nameField.innerText = "Equilateral";
+  } else if (isIsosceles(sideA, sideB, sideC)) {
+    nameField.innerText = "Isosceles";
+  } else {
+    nameField.innerText = "Scalene";
+  }
+  typeField.classList.remove("hidden");
+  
 }
 
 window.onload = function () {
