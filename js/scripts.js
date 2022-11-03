ca79c57ea7d2656781eab47abc7e3e54f660d9e8
Cameron Abel <cameronabel@gmail.com>
2022-11-03 00:30:19 -0700

    add invalid sides error handling

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 
 function isEquilateral(sideA, sideB, sideC) {
   console.log("func eq")
-  return (sideA === sideB && sideA === sideC);
+  return sideA === sideB && sideA === sideC;
 }
 
 function isIsosceles(sideA, sideB, sideC) {
@@ -17,11 +17,24 @@ function isIsosceles(sideA, sideB, sideC) {
   }
 }
 
+function isValidTriangle(sideA, sideB, sideC) {
+  return (
+    sideA + sideB > sideC
+    && sideA + sideC > sideB
+    && sideB + sideB > sideA
+  )
+}
+
+
 function hideResultsAndError () {
   nullP = document.getElementById("null-error");
   typeField = document.getElementById("triangle-type");
+  sideP = document.getElementById("side-error");
   if (!(nullP.classList.contains("hidden"))) {
     nullP.classList.add("hidden");
+  }
+  if (!(sideP.classList.contains("hidden"))) {
+    sideP.classList.add("hidden");
   }
   if (!(typeField.classList.contains("hidden"))) {
     typeField.classList.add("hidden");
@@ -32,6 +45,7 @@ function triangulate(event) {
   event.preventDefault();
   hideResultsAndError ();
   nullP = document.getElementById("null-error");
+  sideP = document.getElementById("side-error");
   typeField = document.getElementById("triangle-type");
   let sideA = document.getElementById("side-a").value;
   let sideB = document.getElementById("side-b").value;
@@ -39,12 +53,19 @@ function triangulate(event) {
 
   if (sideA === "" || sideB === "" || sideC === "") {
     nullP.classList.remove("hidden");
-    return
+    return;
   }
 
   sideA = parseFloat(sideA);
   sideB = parseFloat(sideB);
   sideC = parseFloat(sideC);
+
+  if (!(isValidTriangle(sideA, sideB, sideC))) {
+    sideP.classList.remove("hidden");
+    return;
+  }
+
+
 
   let nameField = document.getElementById("triangle-name");
   if (isEquilateral(sideA, sideB, sideC)) {
